97246fdaae258556e24d6cc0a940f84c5ab73d47
Willian Santos <williansantosd2@gmail.com>
2024-10-09 12:58:50 -0300

    feat: created a next.js boilerplate

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,7 @@ module.exports = {
   // Lint & Prettify TS and JS files
   "*.{js,jsx,ts,tsx}": (filenames) => [
     `prettier --write ${filenames.join(" ")}`,
-    `pnpm lint --fix . ${filenames.join(" --file")}`,
+    `pnpm lint --fix . ${filenames.join(" ")}`,
     // `pnpm test -- --findRelatedTests ${filenames.join(" ")}`,
   ],
 };
